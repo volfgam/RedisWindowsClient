403a5a15134a5e15c47e57e2c9f44eb5b217b014
volfgam <volfgam@gmail.com>
2020-01-17 17:43:30 -0300

    Config user windows

---
+++ UserSettings.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace RedisWindowsClient
+{
+    public partial class UserSettings : Window
+    {
+        public UserSettings()
+        {
+            InitializeComponent();
+        }
+
+        //https://stackoverflow.com/questions/56847571/equivalent-to-usersettings-applicationsettings-in-wpf-dotnet-core
+        //Pesquisa
+    }
+}
